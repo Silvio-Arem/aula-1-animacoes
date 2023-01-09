863c325652bdcd44ff2c06bfcffafd351761248e
Silvio Arem <silvio.arem@gmail.com>
2023-01-09 17:30:19 -0300

    added index.js file and zoomIn animation in h1

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 let tituloQuePula = document.querySelector("h1");
+let tituloQueAumenta = document.querySelector("h1")
 
-tituloQuePula.onmouseover = ()=>{
-    tituloQuePula.classList.add("animate__animated", "animate__bounce");
-}
+// tituloQuePula.onmouseover = ()=>{
+//     tituloQuePula.classList.add("animate__animated", "animate__bounce");
+// }
+
+tituloQueAumenta.onmouseover = ()=>{
+    tituloQueAumenta.classList.add("animate__animated", "animate__zoomIn")
+} 
+
